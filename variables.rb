391ca52d185982500291a8b92c2b51a2e3969244
Carol Rieping <carol.rieping@gmail.com>
2022-11-01 19:56:00 -0600

    Add puts

--- variables.rb
+++ variables.rb
@@ -4,9 +4,24 @@ month = "November"
 day = 1
 zip_code = 80920
 year = 2022
-balance_$ = 12.99
+balance = 12.99
 length_in = 2.75
 width_in = 4.25
 have_pet = false
 test_result = true
 light_on = false
+name = "Holly"
+address = "345_Yellow_Brick_Rd"
+month = "October"
+puts name
+puts address
+puts month
+puts day
+puts zip_code
+puts year
+puts balance
+puts length_in
+puts width_in
+puts have_pet
+puts light_on
+puts test_result
